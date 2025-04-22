f6623384e4b6e83d8326bda3c5095f172d4a125e
Jonathan <jonathanyin.12@gmail.com>
2025-04-22 12:44:13 -0400

    update to handle multiple tasks

--- eval/aggregate_results.py
+++ eval/aggregate_results.py
@@ -3,7 +3,6 @@ import glob
 import json
 import os
 from collections import defaultdict
-from typing import List
 
 import numpy as np
 import pandas as pd
@@ -17,88 +16,101 @@ def check_answer_in_history(history):
         return False
 
 
-def process_run(results_filepath: str, details_filepath: str, tokenizer: AutoTokenizer):
-    results_json = json.load(open(results_filepath))
-    details_df = pd.read_parquet(details_filepath)
-
-    max_new_tokens = results_json["config_general"]["generation_parameters"][
-        "max_new_tokens"
-    ]
-    # Create a new DataFrame with the requested columns
-    df = pd.DataFrame(
-        columns=[
-            "full_prompt",
-            "prediction",
-            "correct_answer",
-            "extracted_answer",
-            "correct",
-            "truncated",
-            "formatted_correctly",
-        ]
-    )
-    # Populate the new DataFrame
-    for i, row in details_df.iterrows():
-        input_prompt = row["full_prompt"]
-        output = row["predictions"][0]
-        correct = row["metrics"]["extractive_match"] == 1
-        answer = row["gold"][0]
-        extracted_answer = row["specifics"]["extracted_predictions"][0]
-
-        full_history = input_prompt + output
-        num_tokens = len(tokenizer.tokenize(full_history))
-
-        # Determine if response is truncated
-        truncated = num_tokens >= max_new_tokens
-
-        # Determine if answer is formatted correctly
-        answer_formatted_correctly = (
-            check_answer_in_history(full_history) if not truncated else False
-        )
-
-        # Add row to new DataFrame
-        df.loc[i] = {
-            "full_prompt": input_prompt,
-            "prediction": output,
-            "correct_answer": answer,
-            "extracted_answer": extracted_answer,
-            "correct": correct,
-            "truncated": truncated,
-            "formatted_correctly": answer_formatted_correctly,
-        }
-
-    tasks = []
+def process_run_results_json(path: str):
+    results_json = json.load(open(path))
+    task_names = []
+    results = {}
     for task_name in results_json["summary_tasks"].keys():
-        tasks.append(task_name)
+        task_names.append(task_name)
+        task_result_dict = results_json["results"][task_name].copy()
 
-    assert len(tasks) == 1, "There should only be one task"
-    task_name = tasks[0]
+        # Remove metrics that are not in the metric_names list (i.e. _stderr values, which are not needed for aggregation)
+        config_task_name = "|".join(task_name.split("|")[:-1])
+        task_metric_names = [
+            metric["metric_name"]
+            for metric in results_json["config_tasks"][config_task_name]["metric"]
+        ]
+        for key in results_json["results"][task_name].keys():
+            if key not in task_metric_names:
+                del task_result_dict[key]
+        results[task_name] = task_result_dict
 
-    config_task_name = next(iter(results_json["config_tasks"].keys()))
-
-    metric_names = [
-        metric["metric_name"]
-        for metric in results_json["config_tasks"][config_task_name]["metric"]
-    ]
-
-    # Remove metrics that are not in the metric_names list (i.e. _stderr values, which are not needed for aggregation)
-    results = results_json["results"][task_name].copy()
-
-    for key in results_json["results"][task_name].keys():
-        if key not in metric_names:
-            del results[key]
-
+    seed = results_json["config_general"]["generation_parameters"]["seed"]
+    del results_json["config_general"]["generation_parameters"]["seed"]
     metadata = {
         "model_name": results_json["config_general"]["model_name"],
+        "seed": seed,
         "generation_parameters": results_json["config_general"][
             "generation_parameters"
         ],
         "start_time": results_json["config_general"]["start_time"],
         "end_time": results_json["config_general"]["end_time"],
         "results": results,
-        "task_name": config_task_name,
-        "task_config": results_json["config_tasks"][config_task_name],
+        "task_names": task_names,
+        "task_config": results_json["config_tasks"],
     }
-    return metadata, df
+    return metadata
+
+
+def process_run(run_info: dict, tokenizer: AutoTokenizer):
+    metadata = process_run_results_json(run_info["result_filepath"])
+
+    task_names = metadata["task_names"]
+
+    response_dfs = {}
+    for details_filepath in run_info["details_filepaths"]:
+        for task_name in task_names:
+            if task_name in details_filepath:
+                details_df = pd.read_parquet(details_filepath)
+
+                max_new_tokens = metadata["generation_parameters"]["max_new_tokens"]
+                # Create a new DataFrame with the requested columns
+                df = pd.DataFrame(
+                    columns=[
+                        "full_prompt",
+                        "prediction",
+                        "correct_answer",
+                        "extracted_answer",
+                        "correct",
+                        "truncated",
+                        "formatted_correctly",
+                    ]
+                )
+                # Populate the new DataFrame
+                for i, row in details_df.iterrows():
+                    input_prompt = row["full_prompt"]
+                    output = row["predictions"][0]
+                    answer = row["gold"][0]
+
+                    # This assumes that the metric is the extractive match metric
+                    correct = row["metrics"]["extractive_match"] == 1
+                    extracted_answer = row["specifics"]["extracted_predictions"][0]
+
+                    full_history = input_prompt + output
+                    num_tokens = len(tokenizer.tokenize(full_history))
+
+                    # Determine if response is truncated
+                    truncated = num_tokens >= max_new_tokens
+
+                    # Determine if answer is formatted correctly
+                    answer_formatted_correctly = (
+                        check_answer_in_history(full_history)
+                        if not truncated
+                        else False
+                    )
+
+                    # Add row to new DataFrame
+                    df.loc[i] = {
+                        "full_prompt": input_prompt,
+                        "prediction": output,
+                        "correct_answer": answer,
+                        "extracted_answer": extracted_answer,
+                        "correct": correct,
+                        "truncated": truncated,
+                        "formatted_correctly": answer_formatted_correctly,
+                    }
+                response_dfs[task_name] = df
+    return metadata, response_dfs
 
 
 def verify_run_metadata(all_results: dict):
@@ -124,29 +136,21 @@ def verify_run_metadata(all_results: dict):
             raise ValueError(f"Run {run_key} has different model name")
 
         # Check generation parameters
-        # Remove seed from generation parameters before comparing
-        generation_parameters_1 = run_info["metadata"]["generation_parameters"].copy()
-        generation_parameters_2 = all_results[first_run_key]["metadata"][
-            "generation_parameters"
-        ].copy()
+        if not json.dumps(
+            run_info["metadata"]["generation_parameters"], sort_keys=True
+        ) == json.dumps(
+            all_results[first_run_key]["metadata"]["generation_parameters"],
+            sort_keys=True,
+        ):
+            raise ValueError(f"Run {run_key} has different generation parameters")
 
-        seed = generation_parameters_1.get("seed")
+        seed = run_info["metadata"]["seed"]
         seeds.add(seed)
 
         # Track which runs use the same seed
         if seed not in seed_to_runs:
             seed_to_runs[seed] = []
         seed_to_runs[seed].append(run_key)
-
-        # Remove seed from both dictionaries if present
-        if "seed" in generation_parameters_1:
-            del generation_parameters_1["seed"]
-        if "seed" in generation_parameters_2:
-            del generation_parameters_2["seed"]
-        if not json.dumps(generation_parameters_1, sort_keys=True) == json.dumps(
-            generation_parameters_2, sort_keys=True
-        ):
-            raise ValueError(f"Run {run_key} has different generation parameters")
 
     if len(seeds) != len(all_results):
         duplicate_seeds = [seed for seed, runs in seed_to_runs.items() if len(runs) > 1]
@@ -161,79 +165,74 @@ def verify_run_metadata(all_results: dict):
 def aggregate_results(runs: dict, tokenizer: AutoTokenizer):
     all_results = {}
     for run_key, run_info in runs.items():
-        metadata, responses_df = process_run(
-            run_info["result_filepath"], run_info["details_filepath"], tokenizer
-        )
+        metadata, responses_dfs = process_run(run_info, tokenizer)
         all_results[run_key] = {
             "metadata": metadata,
-            "responses": responses_df,
+            "responses": responses_dfs,
         }
 
-    # Aggregate metric values across runs
-    metric_values = defaultdict(list)
-    for run_key, result in all_results.items():
-        metadata = result["metadata"]
-        for metric_name, metric_value in metadata["results"].items():
-            metric_values[metric_name].append(metric_value)
+    verify_run_metadata(all_results)
 
-    # Calculate mean and standard deviation for each metric
-    metric_aggregates = {}
-    for metric_name, values in metric_values.items():
-        mean = np.mean(values)
-        std_dev = np.std(values, ddof=0)
-        metric_aggregates[metric_name] = {
-            "mean": round(mean, 4),
-            "std_dev": round(std_dev, 4),
-        }
-
-    response_metadata = defaultdict(list)
-    for run_key, result in all_results.items():
-        responses_df = result["responses"]
-        fraction_truncated = responses_df["truncated"].mean()
-        fraction_formatted_correctly = responses_df["formatted_correctly"].mean()
-        fraction_incorrectly_formatted_non_truncated = (
-            1 - fraction_formatted_correctly - fraction_truncated
-        )
-        response_metadata["truncated_responses"].append(fraction_truncated)
-        response_metadata["incorrectly_formatted_non_truncated_responses"].append(
-            fraction_incorrectly_formatted_non_truncated
-        )
-        response_metadata["formatted_correctly_responses"].append(
-            fraction_formatted_correctly
-        )
-
-    # Calculate mean and standard deviation for each metric
-    aggregated_response_metadata = {}
-    for metric_name, values in response_metadata.items():
-        mean = np.mean(values)
-        std_dev = np.std(values, ddof=0)
-        aggregated_response_metadata[metric_name] = {
-            "mean": round(mean, 4),
-            "std_dev": round(std_dev, 4),
-        }
-
-    # Extract a reference run for metadata
     reference_run = next(iter(all_results.values()))["metadata"]
-
-    # Collect all seeds
-    seeds = {
-        result["metadata"]["generation_parameters"]["seed"]
-        for result in all_results.values()
-    }
+    seeds = {result["metadata"]["seed"] for result in all_results.values()}
+    tasks = reference_run["task_names"]
 
     eval_result = {
-        "task_name": reference_run["task_name"],
+        "tasks": tasks,
         "num_runs": len(all_results),
         "model_name": reference_run["model_name"],
-        "generation_parameters": {
-            k: v
-            for k, v in reference_run["generation_parameters"].items()
-            if k != "seed"
-        },
+        "generation_parameters": reference_run["generation_parameters"],
         "seeds": list(seeds),
-        "metrics": metric_aggregates,
-        "response_metadata": aggregated_response_metadata,
     }
+
+    for task in tasks:
+        # Aggregate metric values across runs
+        metric_values = defaultdict(list)
+        for run_key, result in all_results.items():
+            metadata = result["metadata"]
+            for metric_name, metric_value in metadata["results"][task].items():
+                metric_values[metric_name].append(metric_value)
+
+        # Calculate mean and standard deviation for each metric
+        metric_aggregates = {}
+        for metric_name, values in metric_values.items():
+            mean = np.mean(values)
+            std_dev = np.std(values, ddof=0)
+            metric_aggregates[metric_name] = {
+                "mean": round(mean, 4),
+                "std_dev": round(std_dev, 4),
+            }
+
+        response_metadata = defaultdict(list)
+        for run_key, result in all_results.items():
+            responses_df = result["responses"][task]
+            fraction_truncated = responses_df["truncated"].mean()
+            fraction_formatted_correctly = responses_df["formatted_correctly"].mean()
+            fraction_incorrectly_formatted_non_truncated = (
+                1 - fraction_formatted_correctly - fraction_truncated
+            )
+            response_metadata["truncated_responses"].append(fraction_truncated)
+            response_metadata["incorrectly_formatted_non_truncated_responses"].append(
+                fraction_incorrectly_formatted_non_truncated
+            )
+            response_metadata["formatted_correctly_responses"].append(
+                fraction_formatted_correctly
+            )
+
+        # Calculate mean and standard deviation for each metric
+        aggregated_response_metadata = {}
+        for metric_name, values in response_metadata.items():
+            mean = np.mean(values)
+            std_dev = np.std(values, ddof=0)
+            aggregated_response_metadata[metric_name] = {
+                "mean": round(mean, 4),
+                "std_dev": round(std_dev, 4),
+            }
+
+        eval_result[task] = {
+            "metrics": metric_aggregates,
+            "response_metadata": aggregated_response_metadata,
+        }
 
     return eval_result
 
@@ -278,16 +277,15 @@ def main():
         key = filename.split("results_")[1].split(".")[0]
 
         # Find the parquet file that contains the key in its filename
-        matching_details_filepath = None
+        matching_details_filepaths = []
         for details_filepath in details_files:
             if key in details_filepath:
-                matching_details_filepath = details_filepath
-                break
+                matching_details_filepaths.append(details_filepath)
 
-        if matching_details_filepath:
+        if matching_details_filepaths:
             runs[key] = {
                 "result_filepath": result_filepath,
-                "details_filepath": matching_details_filepath,
+                "details_filepaths": matching_details_filepaths,
             }
         else:
             print(f"Warning: No matching details file found for {result_filepath}")
